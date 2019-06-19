da70e73fcfee4ba45f3de5bc207cf6b46f907b24
Alexander Ege <alexander@ege.com>
2019-06-19 08:49:15 -0700

    Completed assignment functionality goals

--- books_authors_shell/apps/books_authors_shell_app/urls.py
+++ books_authors_shell/apps/books_authors_shell_app/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
-    # url(r'^books$', views.books),
-    url(r'^books/(?P<id>\d+)$', views.books_id),
+    url(r'^book/(?P<id>\d+)$', views.book_description),
     url(r'^add_book$', views.add_book),
     url(r'^authors$', views.authors),
+    url(r'^create_authors$', views.create_author),
     url(r'^add_author/$', views.add_author),
-    url(r'^authors/$', views.authors_id),
+    url(r'^author/(?P<id>\d+)$', views.author_description),
+    url(r'^add_author_to_book$', views.add_author_to_book),
 ]
--- books_authors_shell/apps/books_authors_shell_app/views.py
+++ books_authors_shell/apps/books_authors_shell_app/views.py
@@ -12,12 +12,17 @@ def index(request):
 
     return render(request, "books_authors_shell_app/index.html", context)
 
-#Route for localhost:8000/books
-# def books(request):
-#     pass
-#     return render(request, "books_authors_shell_app/books.html")
+#Route for localhost:8000/books/4 - Pulls book information with id from database
+def book_description(request, id):
+    context = {
+        'books' : models.Book.objects.all(),
+        'book' : models.Book.objects.get(id=id),
+        'authors' : models.Author.objects.all(),
+        'author_list' : models.Book.objects.get(id=id).Author.all(),
+    }
+    return render(request, "books_authors_shell_app/books.html", context, id)
 
-#
+#Route for localhost:8000/add_book - Adds a book to the database
 def add_book(request):
     context = {
         'books' : models.Book.objects.all(),
@@ -26,38 +31,38 @@ def add_book(request):
     models.Book.objects.create(title=request.POST['title'], desc=request.POST['description'])
     return redirect('/')
 
-#Route for localhost:8000/add_author
-def add_author(request):
-    book_id = request.POST['book_id']
-    author_id = request.POST['author_id']
-    models.Book.objects.get(id=book_id).Author.add(Author.objects.get(id=author_id))
-
-    return redirect('/')
-
-#Route for localhost:8000/authors
+#Route for localhost:8000/authors - Render Authors page
 def authors(request):
     context = {
         'authors' : models.Author.objects.all(),
     }
     return render(request, "books_authors_shell_app/authors.html", context)
 
-
+#Route for localhost:8000/add_author
+def add_author(request):
+    book_id = request.POST['book_id']
+    author_id = request.POST['author_id']
+    models.Book.objects.get(id=book_id).Author.add(Author.objects.get(id=author_id))
+    return redirect('/')
 
 #Route for localhost:8000/authors/4
-def authors_id(request):
+def create_author(request):
     models.Author.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], notes=request.POST['notes'])
     return redirect('/authors')
     # return render(request, "books_authors_shell_app/authors.html")
 
-#Route for localhost:8000/books/4
-def books_id(request, id):
+#Render details page about "viewed" author
+def author_description(request, id):
     context = {
-        'books' : models.Book.objects.all(),
-        'book' : models.Book.objects.get(id=id),
-        'authors' : models.Author.objects.all(),
-        'author_list' : models.Book.objects.get(id=id).Author.all(),
+        'author' : models.Author.objects.get(id=id),
+        'book_list' : models.Author.objects.get(id=id).books.all(),
+        'all_books' : models.Book.objects.all(),
     }
+    return render(request, "books_authors_shell_app/author.html", context, id)
 
-    # print(context['books'])
-
-    return render(request, "books_authors_shell_app/books.html", context, id)
+#Add an author to the list of authors for given book
+def add_author_to_book(request):
+    book_id = request.POST['book_id']
+    author_id = request.POST['author_id']
+    models.Author.objects.get(id=author_id).books.add(Book.objects.get(id=book_id))
+    return redirect('/authors')
